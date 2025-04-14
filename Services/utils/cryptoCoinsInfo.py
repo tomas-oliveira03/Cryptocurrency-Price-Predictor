e6b53384c0b217b45e34d8415ba16786eff3e410
Tomas <tumabarbosa@gmail.com>
2025-04-14 16:33:23 +0100

    change crypto and add aux funtions

--- Services/utils/cryptoCoinsInfo.py
+++ Services/utils/cryptoCoinsInfo.py
@@ -1,2 +1,18 @@
 def getTopCoins():
-    return ['BTC', 'ETH', 'USDT', 'XRP', 'BNB', 'SOL', 'USDC', 'DOGE', 'TRX', 'ADA']
+    return ['BTC', 'ETH', 'XRP', 'BNB', 'SOL', 'DOGE', 'TRX', 'ADA']
+
+def getTickerToFullNameMap():
+    return {
+        'BTC': 'bitcoin',
+        'ETH': 'ethereum',
+        'XRP': 'ripple',
+        'BNB': 'binancecoin',
+        'SOL': 'solana',
+        'DOGE': 'dogecoin',
+        'TRX': 'tron',
+        'ADA': 'cardano'
+    }
+    
+def getFullNameToTickerMap():
+    ticker_to_id = getTickerToFullNameMap()
+    return {v: k for k, v in ticker_to_id.items()}
